6972ff788156528e7d46c7b3582c4e4f1f8248d5
Andrea <andrea.giammanco@cern.ch>
2013-11-15 15:40:05 +0100

    another attempt: recreate TrackExtras from scratch

--- FastSimulation/Tracking/plugins/RecoTrackAccumulator.cc
+++ FastSimulation/Tracking/plugins/RecoTrackAccumulator.cc
@@ -40,13 +40,29 @@ void RecoTrackAccumulator::accumulate(edm::Event const& e, edm::EventSetup const
   e.getByLabel(GeneralTrackInputSignal_, tracks);
   e.getByLabel(GeneralTrackExtraInputSignal_, trackExtras);
 
+
   if (tracks.isValid()) {
     for (auto const& track : *tracks) {
       NewTrackList_->push_back(track);
-    }
-    for (auto const& trackExtra : *trackExtras) {
+      // corresponding TrackExtra:
+      //      const reco::TrackExtraRef & trackExtraRef_(track.extra());
+      reco::TrackExtra trackExtra(track.outerPosition(),
+				  track.outerMomentum(),
+				  track.outerOk(),
+				  track.innerPosition(),
+				  track.innerMomentum(),
+				  track.innerOk(),
+				  track.outerStateCovariance(),
+				  track.outerDetId(),
+				  track.innerStateCovariance(),
+				  track.innerDetId(),
+				  track.seedDirection(),
+				  track.seedRef()); 
       NewTrackExtraList_->push_back(trackExtra);
     }
+    //    for (auto const& trackExtra : *trackExtras) {
+    //      NewTrackExtraList_->push_back(trackExtra);
+    //    }
   }
 
 }
@@ -60,12 +76,27 @@ void RecoTrackAccumulator::accumulate(PileUpEventPrincipal const& e, edm::EventS
     e.getByLabel(GeneralTrackExtraInputPileup_, trackExtras);
     
     if (tracks.isValid()) {
-      for (reco::TrackCollection::const_iterator track = tracks->begin();  track != tracks->end();  ++track) {
-	NewTrackList_->push_back(*track);
-      }
-      for (auto const& trackExtra : *trackExtras) {
+      //      for (reco::TrackCollection::const_iterator track = tracks->begin();  track != tracks->end();  ++track) {
+      for (auto const& track : *tracks) {
+	//	NewTrackList_->push_back(*track);
+	NewTrackList_->push_back(track);
+	reco::TrackExtra trackExtra(track.outerPosition(),
+				    track.outerMomentum(),
+				    track.outerOk(),
+				    track.innerPosition(),
+				    track.innerMomentum(),
+				    track.innerOk(),
+				    track.outerStateCovariance(),
+				    track.outerDetId(),
+				    track.innerStateCovariance(),
+				    track.innerDetId(),
+				    track.seedDirection(),
+				    track.seedRef()); 
 	NewTrackExtraList_->push_back(trackExtra);
       }
+      //      for (auto const& trackExtra : *trackExtras) {
+      //	NewTrackExtraList_->push_back(trackExtra);
+      //      }
     }
   }
 
